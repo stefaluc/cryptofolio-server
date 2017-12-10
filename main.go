7b44831fcff0676213875b5efb49570423c373ee
Martin Desrumaux <martin.desrumaux@epitech.eu>
2017-12-10 15:30:47 +0100

    DB according host

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	// init DB
 	var errdb error
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable",
-	os.GetEnv("DB_USER"), os.GetEnv("DB_PASSWORD"), os.GetEnv("DB_NAME"), os.GetEnv("DB_HOST"))
+	os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"))
 	database.DBConn, errdb = sql.Open("postgres", dbinfo)
 	if errdb != nil {
 		panic(errdb)
