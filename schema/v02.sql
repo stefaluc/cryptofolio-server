0fe38c9ac94f206299a7cf2539e5b59c00731e9c
Lucas Stefanski <stefaluc000@gmail.com>
2017-12-13 03:07:39 +0700

    Stored procedures

--- schema/v02.sql
+++ schema/v02.sql
@@ -8,3 +8,21 @@ $$ LANGUAGE plpgsql;
 SELECT add_currency('Bitcoin', 'https://bitcoin.org/img/icons/opengraph.png');
 SELECT add_currency('Ethereum', 'http://files.coinmarketcap.com.s3-website-us-east-1.amazonaws.com/static/img/coins/200x200/ethereum.png');
 SELECT add_currency('Litecoin', 'https://litecoin.org/img/litecoin.svg');
+
+CREATE OR REPLACE FUNCTION transactions_by_date(date_start TIMESTAMP, date_end TIMESTAMP)
+DECLARE ref refcursor;
+BEGIN
+  OPEN ref FOR SELECT * FROM "transaction" WHERE date >= date_start AND date < date_end;
+  RETURN ref;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION user_investment(in_user_id INT)
+DECLARE ref refcursor;
+BEGIN
+  OPEN ref FOR SELECT SUM(price) AS investment
+               FROM "transaction" t, "balance" b
+               WHERE b.user_id=in_user_id AND b.id=t.balance_id;
+  RETURN ref;
+END;
+$$ LANGUAGE plpgsql;
